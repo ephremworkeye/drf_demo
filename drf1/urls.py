7caa5599a56ea0ccac6079a6d7ea66ea722410c2
Ephrem Wube <ephremworkeye@gmail.com>
2021-09-22 01:25:18 -0700

    views, urls, serializers, admin resgisterd

---
+++ drf1/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list_people', views.list_people)
+]
--- drf1/views.py
+++ drf1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Person
+from .serializers import PersonSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def list_people(request):
+    peoples = Person.objects.all()
+    serializer = PersonSerializer(peoples, Many=True)
+    content = {
+        'people': serializer.data
+    }
+
+    return Response(content)
+
