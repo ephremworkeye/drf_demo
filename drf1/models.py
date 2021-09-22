a19c8cdc6ca3d4568f3d261f31f9dc398268bc82
Ephrem Wube <ephremworkeye@gmail.com>
2021-09-22 00:43:10 -0700

    added person model

--- drf1/models.py
+++ drf1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "People"
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
